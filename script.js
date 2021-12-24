92d909f3b380a654f99cb6500d51dba2babdc00f
Tyler Bantley <tyler.bantley@icloud.com>
2021-12-23 22:08:36 -0600

    final deadline commit

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//variables
 var currentHour = parseInt(moment().format('H'));
 var time = [9, 10, 11, 12, 13, 14, 15, 16, 17,];
 var classes = [".9am", ".10am", ".11am", ".12pm", ".1pm", ".2pm", ".3pm", ".4pm", ".5pm",];
@@ -6,7 +7,7 @@ $("#currentDay").text(moment().format('dddd, MMMM D, YYYY'));
 var currentDayCheck = moment().format('dddd, MMMM D, YYYY')
 var notes = ["", "", "", "", "", "", "", "", "",]
 
-
+//asigning past, future, and present classes to rows
 if (currentHour < 10) {
     allFuture();
 }
@@ -17,7 +18,7 @@ else {
     formatTimes()
 }
 
-
+grabData();
 
 
 function allFuture() {
@@ -43,7 +44,7 @@ function formatTimes() {
         $(classes[i]).addClass("future");
     }
 }
-
+//single item save function
 $(".saveBtn").on("click", function () {
     var dI = $(this).data('index');
     notes[dI] = $(classes[dI]).val();
@@ -51,8 +52,7 @@ $(".saveBtn").on("click", function () {
     alert("saved")
 })
 
-grabData();
-
+//restores data to rows from storage
 function grabData() {
     notes = JSON.parse(localStorage.getItem("notes"));
     if (notes == null) {
@@ -67,6 +67,7 @@ function grabData() {
 $(".clear").on("click", function () {
     clearData() 
 })
+//clears data from storage
 function clearData() {
     var confirmDelete = confirm("Are you sure?");
     if (confirmDelete == true) {
@@ -75,9 +76,9 @@ function clearData() {
         grabData();
     }
 }
-
-$("saveAll").on("click", function () {
-    for (i = 0; i < classes. length; i++) {
+//save all items function
+$(".saveAll").on("click", function () {
+    for (i = 0; i < classes.length; i++) {
         notes[i] = $(classes[i]).val();
     }
     localStorage.setItem('notes', JSON.stringify(notes))
